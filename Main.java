2f79bb6c0c8a3ec30dd26a1e43fb623af02cff4f
ODBabalola <u18041494@tuks.co.za>
2022-03-12 18:44:29 +0200

    Date processing

--- Main.java
+++ Main.java
@@ -2,15 +2,17 @@ import java.io.File;    // Import File class
 import java.io.FileNotFoundException;   // Import this class to handle errors
 import java.util.Scanner;   // Import the Scanner class to read text files
 import java.util.ArrayList; // Import the ArrayList class
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("=================================================");
         System.out.println("Program Starting....");
         System.out.println("=================================================");
 
         Scanner userInput = new Scanner(System.in); // Create a Scanner object
-        System.out.println("Enter email text file name, eg. \'email1.txt\':");
+        System.out.println("Enter email text file name, eg. 'email1.txt':");
         String fileName = userInput.nextLine(); // Read user input
         System.out.println("=================================================");
 
@@ -22,6 +24,7 @@ public class Main {
             fromAddress = toAddress = rawDate = "";
             String[] temp, temp2;
             ArrayList<String> sentTos = new ArrayList<String>();
+            Date date = null;
 
             // Reading and processing the input
             while (myReader.hasNextLine()) {
@@ -39,9 +42,21 @@ public class Main {
                 }
                 temp = null;
                 if(data.matches("Date:.*")) {
-                    rawDate = data.substring(5,data.length).trim();
+                    rawDate = data.substring(5,data.length()).trim();
+                    // Instantiating the SimpleDateFormat class
+                    SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z");
+                    // Parsing the given String to Date object
+                    try {
+                        date = formatter.parse(rawDate);
+                        //System.out.println("Date object value: " + date);
+                    } catch (Exception e) {
+                        System.out.println("An error occured.");
+                        e.printStackTrace();
+                    }
+
+
                 }
-                
+
                 temp = null;
                 if (data.matches("To:.*")) {
                     if (data.contains(",")) {
@@ -91,7 +106,7 @@ public class Main {
 
             // Printing out output
             System.out.println("The From Address: " + fromAddress);
-            System.out.println("The Date: " + rawDate);
+            System.out.println("The Date: " + date);
             System.out.print("The To Address: ");
             for (int k = 0; k < sentTos.size(); k++) {
                 System.out.print(sentTos.get(k));
@@ -100,6 +115,7 @@ public class Main {
                 }
             }
             System.out.println();
+
             myReader.close();
         }
         catch (FileNotFoundException e) {
