810b12d346c1698cedc58d4f2bf874d5b77356c3
ODBabalola <u18041494@tuks.co.za>
2022-03-13 02:06:24 +0200

    Modularised the code

--- Main.java
+++ Main.java
@@ -2,29 +2,19 @@ import java.io.File;    // Import File class
 import java.io.FileNotFoundException;   // Import this class to handle errors
 import java.util.Scanner;   // Import the Scanner class to read text files
 import java.util.ArrayList; // Import the ArrayList class
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Date;  // Import the Data class
+import java.text.SimpleDateFormat; // Import this class to format string dates
+import java.text.ParseException; // Import class for parse exception
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("=================================================");
-        System.out.println("Program Starting....");
-        System.out.println("=================================================");
-
-        Scanner userInput = new Scanner(System.in); // Create a Scanner object
-        System.out.println("Enter email text file name, eg. 'email1.txt':");
-        String fileName = userInput.nextLine(); // Read user input
-        System.out.println("=================================================");
+    private static String getFromAddress(String fName) {
+        String address = null;
+        String[] temp;
 
         try {
-            File myObj = new File(fileName);
+            File myObj = new File(fName);
             Scanner myReader = new Scanner(myObj);
-
-            String fromAddress, toAddress, rawDate;
-            fromAddress = toAddress = rawDate = "";
-            String[] temp, temp2;
-            ArrayList<String> sentTos = new ArrayList<String>();
-            Date date = null;
 
             // Reading and processing the input
             while (myReader.hasNextLine()) {
@@ -32,53 +22,91 @@ public class Main {
                 if (data.matches("From:.*")) {
                     if (data.contains("<")) {
                         temp = data.split("<");
-                        fromAddress = temp[1].replace(">", "").trim();
-                    }
-                    else {
+                       address = temp[1].replace(">", "").trim();
+                    } else {
                         temp = data.split(" ");
-                        fromAddress = temp[1].trim();
+                        address = temp[1].trim();
                     }
-
                 }
-                temp = null;
+            }
+
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An Error Occurred.");
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+    private static Date getDate(String fName) {
+        String  rawDate;
+        Date date = null;
+
+        try {
+            File myObj = new File(fName);
+            Scanner myReader = new Scanner(myObj);
+
+            // Reading and processing the input
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
                 if(data.matches("Date:.*")) {
-                    rawDate = data.substring(5,data.length()).trim();
+                    rawDate = data.substring(5).trim();
                     // Instantiating the SimpleDateFormat class
                     SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z");
                     // Parsing the given String to Date object
                     try {
                         date = formatter.parse(rawDate);
                         //System.out.println("Date object value: " + date);
-                    } catch (Exception e) {
-                        System.out.println("An error occured.");
+                    } catch (ParseException e) {
+                        System.out.println("An Error Occurred.");
                         e.printStackTrace();
                     }
-
-
                 }
+            }
 
-                temp = null;
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An Error Occurred.");
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    private static void parseMail(String line, ArrayList<String> s) {
+        String[] temp, temp2;
+
+        temp2 = line.split(",");
+        for (String l : temp2) {
+            temp = l.split("<");
+            if (1 < temp.length) {
+                s.add(temp[1].replace(">", "").trim());
+            }
+        }
+    }
+
+    private static ArrayList<String> getToAddress(String fName) {
+        String[] temp;
+        ArrayList<String> sentTos = new ArrayList<>();
+
+        try {
+            File myObj = new File(fName);
+            Scanner myReader = new Scanner(myObj);
+
+            // Reading and processing the input
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
                 if (data.matches("To:.*")) {
                     if (data.contains(",")) {
-                        temp2 = data.split(",");
-                        for (int i = 0; i < temp2.length; i++) {
-                            temp = temp2[i].split("<");
-                            if (1 < temp.length) {
-                                sentTos.add(temp[1].replace(">", "").trim());
-                            }
-                        }
-                        temp2 = null;
-                        Boolean loop = true;
+                        parseMail(data, sentTos);
+
+                        boolean loop = true;
                         while (myReader.hasNextLine() && loop) {
                             String secondaryData = myReader.nextLine();
                             if (secondaryData.contains(",")) {
-                                temp2 = secondaryData.split(",");
-                                for (int i = 0; i < temp2.length; i++) {
-                                    temp = temp2[i].split("<");
-                                    if (1 < temp.length) {
-                                        sentTos.add(temp[1].replace(">", "").trim());
-                                    }
-                                }
+                                parseMail(data, sentTos);
                             }
                             else if (secondaryData.contains("<")) {
                                 temp = secondaryData.split("<");
@@ -104,24 +132,40 @@ public class Main {
                 }
             }
 
-            // Printing out output
-            System.out.println("The From Address: " + fromAddress);
-            System.out.println("The Date: " + date);
-            System.out.print("The To Address: ");
-            for (int k = 0; k < sentTos.size(); k++) {
-                System.out.print(sentTos.get(k));
-                if (k != sentTos.size() - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-
             myReader.close();
         }
         catch (FileNotFoundException e) {
-            System.out.println("An error occured.");
+            System.out.println("An Error Occurred.");
             e.printStackTrace();
         }
 
+        return sentTos;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=================================================");
+        System.out.println("Program Starting....");
+        System.out.println("=================================================");
+
+        Scanner userInput = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter email text file name, eg. 'email1.txt':");
+        String fileName = userInput.nextLine(); // Read user input
+        System.out.println("=================================================");
+
+        String fromAddress = getFromAddress(fileName);
+        Date date = getDate(fileName);
+        ArrayList<String> S = getToAddress(fileName);
+
+        // Printing out output
+        System.out.println("The From Address: " + fromAddress);
+        System.out.println("The Date: " + date);
+        System.out.print("The To Address: ");
+        for (int k = 0; k < S.size(); k++) {
+            System.out.print(S.get(k));
+            if (k != S.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
