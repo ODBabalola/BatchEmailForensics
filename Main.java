7179ce0e1ca72f54bcd909da379fbff7e900aab8
ODBabalola <u18041494@tuks.co.za>
2022-04-04 10:32:47 +0200

    Implemented edit distance

--- Main.java
+++ Main.java
@@ -211,6 +211,48 @@ public class Main {
         System.out.println("=================================================");
     }
 
+    private static int getEditDistance(String X, String Y) {
+        /* Edit distance formula otherwise known as the Levenshtein distance */
+        int m = X.length();
+        int n = Y.length();
+
+        int[][] T = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            T[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            T[0][j] = j;
+        }
+
+        int cost;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                cost = X.charAt(i - 1) == Y.charAt(j - 1) ? 0: 1;
+                T[i][j] = Integer.min(Integer.min(T[i - 1][j] + 1, T[i][j - 1] + 1),
+                        T[i - 1][j - 1] + cost);
+            }
+        }
+
+        return T[m][n];
+    }
+
+    private static double findSimilarity(String x, String y) {
+        if (x == null || y == null) {
+            throw new IllegalArgumentException("Strings must not be null");
+        }
+
+        double maxLength = Double.max(x.length(), y.length());
+        if (maxLength > 0) {
+            // optionally ignore case if needed
+            return (maxLength - getEditDistance(x, y)) / maxLength;
+        }
+        return 1.0;
+    }
+
+    private static String sentMessage(String fName) {
+        return "";
+    }
+
     public static void main(String[] args) {
         System.out.println("=================================================");
         System.out.println("Program Starting....");
